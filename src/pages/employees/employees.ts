8402fc26626c5bf049ee1811662b15f479178c76
KameronYoung <young.kameron@gmail.com>
2018-03-11 20:42:17 -0500

    employee page can display photo, name, and info. The list of employees is scrollable without scrolling the main content. The word Employees at the top of the list sticks to the top, but when scrolling, the items on the list cover it. Implemented a function to alphabetize the list of employees.

--- src/pages/employees/employees.ts
+++ src/pages/employees/employees.ts
@@ -11,22 +11,27 @@ export class EmployeesPage {
   listPage: any;
   employees: Array<Employee>;
   employee1: Employee;
-  employee2 = new Employee("Carl", 4321, "http://www.math.uni-frankfurt.de/~person/_4170854.jpg");
+  employee2 = new Employee("Carl", 4321,"Manager","$50/hr","(608) 345-1209", "http://www.math.uni-frankfurt.de/~person/_4170854.jpg");
   employee3: Employee;
   selectedEmployee: Employee;
 
   constructor(public navCtrl: NavController){
       this.employees = new Array<Employee>();
       
-      this.employee1 = new Employee("Kevin", 1234,"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxhJ8HaQ88jGA0Ws2WTCnI4DzSgMzvEXk4qdbQVbCAiKyP9yGl");
-      this.employee3 = new Employee("Tina", 5678, "https://cdn2.goabroad.com/images/program_content/5-tips-for-teaching-english-abroad-as-a-person-of-color-2-1462426680.jpg")
-      this.selectedEmployee = this.employee1;
+      this.employee1 = new Employee("Kevin", 1234,"Server","$5/hr","(608) 329-4565","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxhJ8HaQ88jGA0Ws2WTCnI4DzSgMzvEXk4qdbQVbCAiKyP9yGl");
+      this.employee3 = new Employee("Tina", 5678,"Chef","$500/hr","(414) 921-4980", "https://cdn2.goabroad.com/images/program_content/5-tips-for-teaching-english-abroad-as-a-person-of-color-2-1462426680.jpg")
       this.employees.push(this.employee1, this.employee2, this.employee3);
+      this.employees.sort(this.sortByName);
+      this.selectedEmployee = this.employees[0];
   }
 
   selectEmployee(myEvent, employee)
   {
     this.selectedEmployee = employee;
+  }
+  sortByName(a: Employee, b:Employee): number
+  {
+    return a.getName().localeCompare(b.getName());
   }
   
 }
@@ -36,12 +41,18 @@ export class Employee {
   name: string;
   ID: number;
   imageSrc: string;
+  title: string;
+  pay: string;
+  phone: string;
 
-  constructor(name: string, ID: number, imageSrc: string)
+  constructor(name: string, ID: number, title: string, pay: string, phone: string, imageSrc: string)
   {
       this.name = name;
       this.ID = ID;
       this.imageSrc = imageSrc;
+      this.title = title;
+      this.pay = pay;
+      this.phone = phone;
   }
 
   getName(): string
@@ -53,6 +64,21 @@ export class Employee {
   {
     return this.imageSrc;
   }
-
+  getID(): number
+  {
+    return this.ID;
+  }
+  getTitle(): string
+  {
+    return this.title;
+  }
+  getPhone(): string
+  {
+    return this.phone;
+  }
+  getPay(): string
+  {
+    return this.pay;
+  }
 }
 
